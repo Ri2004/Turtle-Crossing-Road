1f9588115fc96bc32d043a1a4b5f945538a40ad5
Rishabh Chatterjee <rishabhchatterjee813@gmail.com>
2025-07-02 16:01:14 +0530

    Player Turtle Created, and it moves from bottom of screen to top of screen

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+turtle_player = Player()
+carTurtle = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(key="Up", fun=turtle_player.move_up)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
