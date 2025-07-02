1191a4c6d6edeef4d4f2dcd2b4bac739aaa5e6c2
Rishabh Chatterjee <rishabhchatterjee813@gmail.com>
2025-07-02 16:06:29 +0530

    One car is created at center, and scoreboard is created

--- car_manager.py
+++ car_manager.py
@@ -6,6 +6,10 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color(random.choice(COLORS))
+        self.shapesize(stretch_len=2, stretch_wid=1)
+        self.shape("square")
 
--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,19 @@
 from turtle import Turtle
 
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 24, "bold")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(x = -270, y = 250)
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(arg=f"Level: {self.score}", font=FONT)
+        self.score += 1
